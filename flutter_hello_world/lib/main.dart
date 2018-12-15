a762e2a587dcc918b800d52487074416d81c320b
CHINNATHAMBI, KARTHICK (kc3503) <kc3503@intl.att.com>
2018-12-15 22:13:00 +0530

    A Flutter App with Just a container

---
+++ flutter_hello_world/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new HelloApp());
+}
+
+class HelloApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.white,
+    );
+  }
+}
