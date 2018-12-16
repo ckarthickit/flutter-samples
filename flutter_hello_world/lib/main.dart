87e7e1f738e9dd3f24aa08c7c42a4088eaafafe3
CHINNATHAMBI, KARTHICK (kc3503) <kc3503@intl.att.com>
2018-12-16 08:01:14 +0530

    Renaming HelloApp to MyHomePage as it is really a page

--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new HelloApp());
+  runApp(new MyHomePage());
 }
 
-class HelloApp extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
