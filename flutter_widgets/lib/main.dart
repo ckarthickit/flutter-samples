abf9b610ba6492ad238d3caad7a9912d71f509ba
CHINNATHAMBI, KARTHICK (kc3503) <kc3503@intl.att.com>
2018-12-16 08:37:31 +0530

    BaseCode for Flutter Widgets

---
+++ flutter_widgets/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  new MaterialApp(
+    home: WidgetHome(),
+  )
+  );
+
+  class WidgetHome extends StatelessWidget {
+
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        body: new Container(),
+      );
+    }
+  }
