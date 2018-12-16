5b5f10b4c8fcf0ed5f4b6174b87be49438a37f00
CHINNATHAMBI, KARTHICK (kc3503) <kc3503@intl.att.com>
2018-12-16 11:07:02 +0530

    Using Maps to Map a ListItem to route

--- flutter_widgets/lib/main.dart
+++ flutter_widgets/lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/text_widget.dart';
 
-var widgetList = ['Text'];
+var widgetNames = [
+  'Text',
+  'Text2',
+  ];
+var widgets = [
+  (BuildContext context) => TextWidget(),
+  (BuildContext context) => TextWidget(),
+  ];
 
 void main() => runApp(WidgetApp());
 
-  class WidgetApp extends StatelessWidget {
+class WidgetApp extends StatelessWidget {
     Widget build(BuildContext context) {
+      Map<String,WidgetBuilder> widgetRoutes = Map.fromIterables(widgetNames, widgets);
+      for(int i=0; i< widgetNames.length; i++) {
+        
+      }
       return MaterialApp(
         title: "Flutter Widgets Demo",
         debugShowCheckedModeBanner: false,
@@ -14,9 +25,7 @@ void main() => runApp(WidgetApp());
           primarySwatch: Colors.deepOrange
         ),
         home: WidgetHome(),
-        routes: <String,WidgetBuilder>{
-          widgetList[0] : (BuildContext context) => TextWidget(),
-        },
+        routes: widgetRoutes,
       );
     }
   }
@@ -25,19 +34,19 @@ void main() => runApp(WidgetApp());
 
     @override
     Widget build(BuildContext context) {
-      print('Building $widgetList');
+      print('Building $widgetNames');
       return new Scaffold(
         appBar: AppBar(
           title: Text("Widgets Playground"),
         ),
         body: new ListView.builder(
-          itemCount: widgetList.length,
+          itemCount: widgetNames.length,
           itemBuilder: (build, index){
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
               child: InkWell(
                 onTap: (){
-                  Navigator.of(context).pushNamed('${widgetList[index]}');
+                  Navigator.of(context).pushNamed('${widgetNames[index]}');
                 },
                 child: Card(
                   elevation: 5.0,
@@ -47,7 +56,7 @@ void main() => runApp(WidgetApp());
                     padding: EdgeInsets.all(16.0),
                     child: Row(
                       children: <Widget>[
-                        Text(widgetList[index], style: TextStyle(color: Colors.black, fontSize: 20.0),),
+                        Text(widgetNames[index], style: TextStyle(color: Colors.black, fontSize: 20.0),),
                         Icon(Icons.keyboard_arrow_right)
                       ],
                     ),
