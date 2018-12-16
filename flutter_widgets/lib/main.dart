7f56ecd653ffbb47881f4178c69fa4db7e0758aa
CHINNATHAMBI, KARTHICK (kc3503) <kc3503@intl.att.com>
2018-12-16 08:57:48 +0530

    Widget Demo Base UI Code

--- flutter_widgets/lib/main.dart
+++ flutter_widgets/lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  new MaterialApp(
-    home: WidgetHome(),
-  )
-  );
+void main() => runApp(WidgetApp());
+
+  class WidgetApp extends StatelessWidget {
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: "Flutter Widgets Demo",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange
+        ),
+        home: WidgetHome(),
+        routes: <String,WidgetBuilder>{
+        },
+      );
+    }
+  }
 
   class WidgetHome extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
+      var widgetList = [];
+      print(widgetList);
       return new Scaffold(
+        appBar: AppBar(
+          title: Text("Widgets Playground"),
+        ),
         body: new Container(),
       );
     }
