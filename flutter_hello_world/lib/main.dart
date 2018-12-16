bb15ccd1bf5bb05c1355f9d748c003f58fd8d552
CHINNATHAMBI, KARTHICK (kc3503) <kc3503@intl.att.com>
2018-12-16 08:25:35 +0530

    Adding Title/Theme to Material App, Adding Scaffold Structure for HomePage

--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
+    title: "Hello App",
     //without home we will get an assertion error for "navigatorObservers != null"
     home: MyHomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.amber
+    ),
   ));
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      color: Colors.white,
+    /*
+        Scaffold contains the basic material design visual layout structure
+        like AppBar , Floating Action Button , Snackbar, drawer etc.,
+     */
+    return new Scaffold(
+      body: Container(
+        color: Colors.white,
+      ),
     );
   }
 }
